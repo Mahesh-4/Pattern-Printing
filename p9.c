ee28d8ed708483cef1d755c5c4280709187ba090
Mahesh Sonawane <111640831+Mahesh-4@users.noreply.github.com>
2023-03-16 19:54:54 +0530

    Add files via upload

---
+++ p9.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+int main(){
+	int rows = 0, columns = 0, i,j,no=2,odd=3;
+
+	printf("\nEnter the number of rows: ");
+	scanf("%d",&rows);
+
+	printf("\nEnter the number of columns: ");
+	scanf("%d",&columns);
+
+	printf("\n");
+
+	for(i=0;i<rows;i++){
+
+		for(j=0;j<columns;j++){
+
+			printf("%d	",no);
+			no+=odd;
+			odd+=2;
+		}
+
+		printf("\n");
+	}
+
+}
